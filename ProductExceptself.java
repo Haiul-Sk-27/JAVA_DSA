2acb42b935122e82a9ea033a57740216cb4240cf
Haiul SK <Haiulsk017@gmail.com>
2025-11-10 07:25:34 +0530

    JAVA_DSA

---
+++ ProductExceptself.java
@@ -0,0 +1,29 @@
+public class ProductExceptself{
+    public static int[] productExceptSelf(int[] nums){
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        int prefix = 1;
+        for(int i = 0;i<n;i++){
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+        
+        int sufix = 1;
+        for(int i = n-1;i>=0;i--){
+            result[i] *= sufix;
+            sufix*=nums[i];
+        }
+        
+        return result;
+    } 
+    
+    public static void main(String[] args){
+        int[] nums = {1, 2, 3, 4};
+        int[] result = productExceptSelf(nums);
+        
+        for(int val : result){
+            System.out.print(val + " ");
+        }
+    }
+}
