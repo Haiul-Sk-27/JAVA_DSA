454a4f74b46bd2ad6423911cdbc741b0fcdb9fed
Haiul SK <Haiulsk017@gmail.com>
2025-11-13 13:58:24 +0530

    JAVA_DSA

---
+++ ValidPalindrome.java
@@ -0,0 +1,24 @@
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+
+            if (Character.toLowerCase(s.charAt(i++)) != Character.toLowerCase(s.charAt(j--))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("madam"));
+        System.out.println(isPalindrome("racecar"));     
+        System.out.println(isPalindrome(" level"));              
+    }
+}
