27748e28b2004c6bd5c6328dbf4c9561cc646df1
Haiul SK <Haiulsk017@gmail.com>
2025-11-13 14:15:53 +0530

    JAVA_DSA

---
+++ ValidAnagram.java
@@ -0,0 +1,26 @@
+public class ValidAnagram{
+    public static boolean isAnagram(String s,String t){
+        if(s.length() != t.length()) return true;
+        
+        int[] freq = new int [26];
+        
+        for(char c : s.toCharArray()){
+            freq[c-'a']++;
+        }
+        
+        for(char c : t.toCharArray()){
+            freq[c-'a']--;
+        }
+        
+        for(int f : freq){
+            if(f!=0) return false;
+        }
+        
+        return true;
+    }
+    
+    public static void main(String[] args){
+        System.out.println(isAnagram("anagram", "nagaram")); // true
+        System.out.println(isAnagram("rat", "car"));
+    }
+}
